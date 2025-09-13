aa74916da21c7947d8b72236dd04309b7ece0719
chLi <565353780@qq.com>
2025-09-13 16:17:08 +0800

    make triline support any query coords dim

--- occ_vae/Test/triline.py
+++ occ_vae/Test/triline.py
@@ -9,16 +9,18 @@ def test():
     feat_num = 512
     feat_dim = 32
 
-    query_num = 16
+    query_occ_dim = 128
 
     feats = torch.randn(
         [batch_size, 3, feat_num, feat_dim]
-    )  # [B, 3, feat_num, feat_dim]
+    ).cuda()  # [B, 3, feat_num, feat_dim]
 
     triline = Triline(feats)
 
-    for _ in trange(1000):
-        coords = torch.rand(batch_size, query_num, 3) - 0.5
+    for _ in trange(100):
+        coords = (
+            torch.rand(batch_size, query_occ_dim, query_occ_dim, query_occ_dim, 3) - 0.5
+        ).cuda()
 
         feat = triline.query(coords)
 
