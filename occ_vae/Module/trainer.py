92d82e824e3f91e9e3ba2f1fd87c53ea03d5ca4e
chLi <565353780@qq.com>
2025-09-19 15:33:53 +0800

    fix zero acc calculating bug

--- occ_vae/Module/trainer.py
+++ occ_vae/Module/trainer.py
@@ -135,7 +135,7 @@ class Trainer(BaseTrainer):
         positive_acc = (
             positive_pred_occ > 0.5
         ).sum().item() / positive_pred_occ.numel()
-        zero_acc = (zero_pred_occ > 0.5).sum().item() / zero_pred_occ.numel()
+        zero_acc = (zero_pred_occ < 0.5).sum().item() / zero_pred_occ.numel()
 
         loss_dict = {
             "Loss": loss,
