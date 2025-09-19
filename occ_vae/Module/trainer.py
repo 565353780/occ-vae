4e9933ca7dd80f0cf0700358843606c19788238c
chLi <565353780@qq.com>
2025-09-19 15:32:23 +0800

    add acc metric

--- occ_vae/Module/trainer.py
+++ occ_vae/Module/trainer.py
@@ -132,10 +132,17 @@ class Trainer(BaseTrainer):
 
         loss = loss_positive_occ + loss_zero_occ + loss_kl
 
+        positive_acc = (
+            positive_pred_occ > 0.5
+        ).sum().item() / positive_pred_occ.numel()
+        zero_acc = (zero_pred_occ > 0.5).sum().item() / zero_pred_occ.numel()
+
         loss_dict = {
             "Loss": loss,
             "LossPositiveOcc": loss_positive_occ,
             "LossZeroOcc": loss_zero_occ,
+            "PositiveAcc": positive_acc,
+            "ZeroAcc": zero_acc,
             "LossKL": loss_kl,
         }
 
