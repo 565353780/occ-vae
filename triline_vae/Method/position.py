6b67397c35268bd28875673fbecba1c737026283
chLi <565353780@qq.com>
2025-09-26 14:26:43 +0800

    fix triline field size bug

--- triline_vae/Method/position.py
+++ triline_vae/Method/position.py
@@ -9,6 +9,6 @@ def deltas_to_positions(deltas: torch.Tensor) -> torch.Tensor:
 
     pos = cum / total  # 归一化到0~1
     pos = F.pad(pos, (1, 0), value=0.0)  # 在最后一维左侧填充0作为起点
-    pos = pos - 0.5  # 映射到[-0.5, 0.5]
+    pos = pos * 2.0 - 1.0  # 映射到[-0.5, 0.5]
 
     return pos
